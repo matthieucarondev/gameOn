766668e52086b625a9dccf83931396cf4f407303
Matthieu Caron <104826273+matthieucarondev@users.noreply.github.com>
2023-06-19 14:13:00 +0200

    closemodal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,6 +12,7 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
@@ -19,5 +20,6 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
 }
-
-
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none";}
