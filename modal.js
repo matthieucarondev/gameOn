d637374cfac2721b9cb7ba2ce5f7504ad4af2dad
Matthieu Caron <104826273+matthieucarondev@users.noreply.github.com>
2023-07-17 20:58:07 +0200

    finish

--- modal.js
+++ modal.js
@@ -14,6 +14,13 @@ const formData = document.querySelectorAll('.formData');
 const closeForm = document.getElementById('close');
 const closeMessage = document.getElementById('closeMessage');
 const allData = document.getElementById('allData');
+const firstNameMessage = document.getElementById('firstnameError');
+const lastNameMessage = document.getElementById('lastnameError');
+const emailMessage = document.getElementById('emailError');
+const birthdatMessage = document.getElementById('birthdateError');
+const quantityMessage = document.getElementById('quantityError');
+const locationMessage = document.getElementById('locationMessage');
+const checkboxMessage = document.getElementById('checkboxMessage');
 
 
 // launch modal event
@@ -22,15 +29,36 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+
+  firstNameMessage.innerHTML = "";
+  firstName.style.border = '2px solid white';
+
+     lastNameMessage.innerHTML = "";
+    lastName.style.border = '2px solid white';
+
+    emailMessage.innerHTML = "";
+    email.style.border = '2px solid white';
+
+    birthdatMessage.innerHTML = "";
+    birthdate.style.border = '2px solid white';
+
+    quantityMessage.innerHTML = "";
+    numberTournoi.style.border = '2px solid white';
+
+    locationMessage.innerHTML = "";
+
+    checkboxMessage .innerHTML = "";
+    
 }
 // close modal form & reset
 
 function closeModal() {
   modalbg.style.display = "none";
   allData.reset();
+  
 }
-delete errors ;
-closeForm.addEventListener("click", closeModal);
+
+closeForm.addEventListener("click", closeModal );
 
 //    Id formulaire
 
@@ -41,7 +69,6 @@ let birthdate = document.getElementById('birthdate');
 let numberTournoi = document.getElementById('quantity');
 let locations = document.getElementsByClassName('location');
 let checkbox = document.getElementById('checkbox1');
-let checkboxTwo = document.getElementById('checkbox2');
 let comfirmForm = document.getElementById("valid-form");
 
 //Regex
@@ -75,11 +102,11 @@ allData.addEventListener("submit", function (event) {
 function validateForm() {
  const isfirstNameValid = firstNameValid()
  const islastNameValid =lastNameValid()
-const isemailValid =  emailValid()
+ const isemailValid =  emailValid()
  const isbirthdateValid = birthdateValid()
-  const isquantityValid = quantityValid()
+ const isquantityValid = quantityValid()
  const islocationValid =locationValid()
-  const ischeckValid= checkValid()
+ const ischeckValid= checkValid()
  return isfirstNameValid && islastNameValid && isemailValid && isbirthdateValid && isquantityValid && islocationValid && ischeckValid }
    
 
@@ -93,8 +120,8 @@ si ce n'est pas le cas :
  - on affiche un message d'erreur et le cadre du champs devient rouge
  */
 function firstNameValid() {
-   let firstNameMessage = document.getElementById("firstnameError");
-   const messageError = firstNameMessage.innerHTML;
+  
+   
   if (!nameRegEx.test(firstName.value)) {
     firstNameMessage.innerHTML = errors.errorName;
     firstNameMessage.style.color= '#ff0000';
@@ -106,8 +133,7 @@ function firstNameValid() {
     return true;
   }
 }
-function lastNameValid() {
-   let lastNameMessage = document.getElementById("lastnameError");
+function lastNameValid() { 
   if (!nameRegEx.test(lastName.value)) {
     lastNameMessage.innerHTML = errors.errorName;
     lastNameMessage.style.color= '#ff0000';
@@ -127,7 +153,7 @@ si ce n'est pas le cas:
 - on affiche un message d'erreur et le cadre du champs devient rouge
 */
 function emailValid() {
-   let emailMessage = document.getElementById("emailError");
+  
   if (!emailRegEx.test(email.value)) {
     emailMessage.innerHTML = errors.errorEmail;
     emailMessage.style.color ='#ff0000';
@@ -157,7 +183,7 @@ function birthdateValid() {
   const diffTime = today.getTime() - vari.getTime();//diff entre les date
   const diffDays = diffTime / (1000 * 3600 * 24); // Conversion en jours
   const age = diffDays / 365.25; // Conversion en années
-  let birthdatMessage = document.getElementById('birthdateError');
+  
   if (!birthRGEX.test(birthdate.value)) {
     birthdatMessage.innerHTML = errors.birthdateError;
      birthdatMessage.style.color='#ff0000';
@@ -181,7 +207,7 @@ function birthdateValid() {
  - on affiche un message d'erreur et le cadre du champs devient rouge
  */
 function quantityValid() { 
-  let quantityMessage = document.getElementById('quantityError');
+ 
   if (numberTournoi.value.length < 1) {
     quantityMessage.innerHTML = errors.errorQuantity;
     quantityMessage.style.color='#ff0000';
@@ -200,18 +226,15 @@ function quantityValid() {
  - on affiche un message d'erreur
  */
 function locationValid() {
-  const result = Array.from(locations).filter(
-    (loca) => loca.checked === true);
-    let locationMessage = document.getElementById('locationMessage');
-  if (result.length <= 0) {
+ for (let i = 0; i < locations.length; i++) {
+        if (locations[i].checked) {
+                locationMessage.innerHTML = "";
+            return true;
+        }
     locationMessage.innerHTML = errors.errorLocation;
     locationMessage.style.color='#ff0000';
-    locations.style.background ='#ff0000 ';
     return false;
-  } else {
-      locationMessage.innerHTML = "";
-    return true;
-  }
+}
 }
 // checkbox
 /* On vérifie  si :
@@ -219,14 +242,13 @@ function locationValid() {
 - on affiche un message d'erreur
 function checkbox */
 function checkValid() {
-   let checkboxMessage = document.getElementById("checkboxMessage");
-  if (checkbox.checked) {
- checkboxMessage .innerHTML = "";
+  if (!checkbox.checked) {
+ checkboxMessage.innerHTML = errors.errorCheckbox;
+     checkboxMessage.style.color='#ff0000';
      return false;
   } else {
-     checkboxMessage.innerHTML = errors.errorCheckbox;
-     checkboxMessage.style.color='#ff0000';
-   return true;
+checkboxMessage .innerHTML = "";
+     return true;
   }
 }
 
